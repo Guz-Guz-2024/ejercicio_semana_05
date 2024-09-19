157eb6356387b26c55be1390ae180ca51798d5e8
Augusto Pascual <pascualzevallosc@iestptrujillo.net>
2024-09-19 11:19:38 -0500

    Mi primer commit

---
+++ noche/Controllers/Rectangulo.cs
@@ -0,0 +1,31 @@
+﻿namespace noche.Controllers
+{
+    public class Rectangulo
+    {
+
+        //propiedades o atributos
+        double baser;
+        double alturar;
+
+        //constructor por defecto
+        public Rectangulo()
+        {
+            baser = 0;
+            alturar = 0;
+        }
+
+        //constructor parametros
+        public Rectangulo(double pbaser, double palturar)
+        {
+            baser = pbaser;
+            alturar = palturar;
+        }
+
+        public double devolverarea()
+        {
+            return baser * alturar;
+        }
+
+
+    }
+}
